30d58855b7fe185bb7e178e46b6c944cf608dc1d
MONVILLE Pierre Francois <pierre-francois.monville@soprasteria.com>
2018-05-09 17:51:19 +0200

    Revert "Revert "add to_datetime""
    This reverts commit 93ae980dfe053c80ad459d91d24a9ce6f04b0351.

--- whr/whole_history_rating.py
+++ whr/whole_history_rating.py
@@ -20,6 +20,8 @@ class Base:
 		self.players = {}
 
 	def print_ordered_ratings(self):
+		"""displays all ratings for each player (for each of his playing days) ordered
+		"""
 		players = [x for x in self.players.values() if len(x.days) > 0]
 		players.sort(key=lambda x: x.days[-1].gamma())
 		for p in players:
@@ -27,6 +29,11 @@ class Base:
 				print("{} => {}".format(p.name, [x.elo() for x in p.days]))
 
 	def get_ordered_ratings(self):
+		"""gets all ratings for each player (for each of his playing days) ordered
+		
+		Returns:
+		    list[list[float]]: for each player and each of his playing day, the corresponding elo
+		"""
 		result = []
 		players = [x for x in self.players.values() if len(x.days) > 0]
 		players.sort(key=lambda x: x.days[-1].gamma())
@@ -36,6 +43,13 @@ class Base:
 		return result
 
 	def log_likelihood(self):
+		"""gets the likelihood of the current state
+
+		the more iteration you do the higher the likelihood becomes
+		
+		Returns:
+		    float: the likelihood
+		"""
 		score = 0.0
 		for p in self.players.values():
 			if len(p.days) > 0:
@@ -43,15 +57,31 @@ class Base:
 		return score
 
 	def player_by_name(self,name):
+		"""gets the player object corresponding to the name
+		
+		Args:
+		    name (str): the name of the player
+		
+		Returns:
+		    Player: the corresponding player
+		"""
 		if self.players.get(name, None) is None:
 			self.players[name] = Player(name, self.config)
 		return self.players[name]
 
 	def ratings_for_player(self,name):
+		"""gets all rating for each day played for the player
+		
+		Args:
+		    name (str): the player's name
+		
+		Returns:
+		    list[list[int,float,float]]: for each day, the time_step the elo the uncertainty
+		"""
 		player = self.player_by_name(name)
 		return [[d.day, round(d.elo()), round(d.uncertainty*100)] for d in player.days]
 
-	def setup_game(self,black,white,winner,time_step,handicap,extras={}):
+	def _setup_game(self,black,white,winner,time_step,handicap,extras={}):
 		if black == white:
 			raise(AttributeError("Invalid game (black player == white player)"))
 			return None
@@ -61,10 +91,23 @@ class Base:
 		return game
 
 	def create_game(self, black, white, winner, time_step, handicap, extras = {}):
-		game = self.setup_game(black, white, winner, time_step, handicap, extras)
-		return self.add_game(game)
+		"""creates a new game to be added to the base
+		
+		Args:
+		    black (str): the black name
+		    white (str): the white name
+		    winner (str): "B" if black won, "W" if white won
+		    time_step (int): the day of the match from origin
+		    handicap (float): the handicap (in elo)
+		    extras (dict, optional): extra parameters
+		
+		Returns:
+		    Game: the added game
+		"""
+		game = self._setup_game(black, white, winner, time_step, handicap, extras)
+		return self._add_game(game)
 
-	def add_game(self, game):
+	def _add_game(self, game):
 		game.white_player.add_game(game)
 		game.black_player.add_game(game)
 		if game.bpd is None:
@@ -73,13 +116,26 @@ class Base:
 		return game
 
 	def iterate(self, count):
+		"""do a number of "count" iterations of the algorithm
+		
+		Args:
+		    count (int): the number of iterations desired
+		"""
 		for _ in range(count):
-			self.run_one_iteration()
+			self._run_one_iteration()
 		for name, player in self.players.items():
 			player.update_uncertainty()
-		return None
 
-	def auto_iterate(self, time_limit = 10):
+	def auto_iterate(self, time_limit = 10, precision = 10E-3):
+		"""Summary
+		
+		Args:
+		    time_limit (int, optional): the maximal time after which no more iteration are launched
+		    precision (float, optional): the precision of the stability desired
+		
+		Returns:
+		    tuple(int, bool): the number of iterations and True if it has reached stability, False otherwise
+		"""
 		start = time.time()
 		self.iterate(10)
 		a = self.get_ordered_ratings()
@@ -88,21 +144,44 @@ class Base:
 			self.iterate(10)
 			i += 10
 			b = self.get_ordered_ratings()
-			if self.test_stability(a,b):
-				return i
+			if self._test_stability(a,b, precision):
+				return i, True
 			if time.time() - start > time_limit:
-				return False
+				return i, False
 			a = b
 
-	def test_stability(self,v1,v2):
+	def _test_stability(self,v1,v2, precision = 10E-3):
+		"""tests if two lists of lists of floats are equal but a certain precision
+		
+		Args:
+		    v1 (list[list[float]]): first list containing ints
+		    v2 (list[list[float]]): second list containing ints
+		    precision (float, optional): the precision after which v1 and v2 are not equal
+		
+		Returns:
+		    bool: True if the two lists are close enought, False otherwise
+		"""
 		v1 = [x for y in v1 for x in y]
 		v2 = [x for y in v2 for x in y]
 		for x1,x2 in zip(v1,v2):
-			if abs(x2-x1) > 10E-3:
+			if abs(x2-x1) > precision:
 				return False
 		return True
 
-	def probability_future_match(self, black, white, handicap, extras = {}):
+	def probability_future_match(self, black, white, handicap = 0, extras = {}):
+	  """gets the probability of winning for an hypothetical match against black and white
+
+	  displays the probability of winning for black and white in percent rounded to the second decimal
+	  
+	  Args:
+	      black (str): black's name
+	      white (str): white's name
+	      handicap (int, optional): the handicap (in elo)
+	      extras (dict, optional): extra parameters
+	  
+	  Returns:
+	      tuple(int,int): the probability between 0 and 1 for black first then white
+	  """
 	  # Avoid self-played games (no info)
 	  if black == white:
 	    raise(AttributeError("Invalid game (black player == white player)"))
@@ -125,13 +204,36 @@ class Base:
 	  game.wpd = new_pday_white
 	  game.bpd = new_pday_black
 	  print("win probability: {}:{:.2f}%; {}:{:.2f}%".format(black,game.black_win_probability(),white,game.white_win_probability()))
+
+	  bpd = black_player.days[-1].gamma()
+	  wpd = white_player.days[-1].gamma()
+	  self.bpd.gamma()/(self.bpd.gamma() + self.opponents_adjusted_gamma(self.black_player))
+
 	  return game.black_win_probability(),game.white_win_probability()
 
-	def run_one_iteration(self):
+	def _run_one_iteration(self):
+		"""runs one iteration of the whr algorithm
+		"""
 		for name,player in self.players.items():
 			player.run_one_newton_iteration()
 
 	def load_games(self, games):
+		"""loads all games at once
+		
+		given a string representing the path of a file or a list of string representing all games,
+		this function loads all games in the base
+		all match must comply to this format:
+			"black_name white_name winner time_step handicap extras"
+			black_name is required
+			white_name is required
+			winner is B or W is required
+			time_step is required
+			handicap is optional (default 0)
+			extras is a dict {} and optional
+
+		Args:
+		    games (str|list[str]): Description
+		"""
 		data = None
 		if isinstance(games, str):
 			with open(games, 'r') as f:
@@ -139,16 +241,33 @@ class Base:
 		else:
 			data = games
 		for line in data:
-			black, white, winner, time_step, handicap, *extras = line.split()
+			handicap = 0
+			extras = None
+			arguments = line.split()
+			if len(arguments) == 6:
+				black, white, winner, time_step, handicap, extras = line.split()
+			if len(arguments) == 5:
+				black, white, winner, time_step, last = line.split()
+				try:
+					eval_last = eval(last)
+					if isinstance(eval_last,dict):
+						extras = eval_last
+					elif isinstance(eval_last,int):
+						handicap = eval_last
+					else:
+						raise(AttributeError("loaded game must have this format: 'black_name white_name winner time_step handicap extras' with handicap and extras optional. the handicap|extras argument is: {}".foramt(last)))
+				except Exception as e:
+					raise(AttributeError("the last argument couldn't be evaluated as an int or a dict: {}".format(last)))
+
+			if len(arguments) == 4:
+				black, white, winner, time_step = line.split()
 			time_step, handicap = int(time_step), int(handicap)
-			self.create_game(black, white, winner, time_step, handicap, extras=extras[0] if len(extras) > 0 else None)
-
-
+			self.create_game(black, white, winner, time_step, handicap, extras=extras)
 
 
 if __name__ == "__main__":
 	whr = Base()
-	games = ["shusaku shusai B 1 0", "shusaku shusai W 2 0", "shusaku shusai W 3 0", "shusaku PF W 3 0"]
+	games = ["shusaku shusai B 1", "shusaku shusai W 2 0", "shusaku shusai W 3 {'w2':300}", "shusaku PF W 3 0 {'w2':300}"]
 	# whr.create_game("shusaku", "shusai", "B", 1, 0)
 	# whr.create_game("shusaku", "shusai", "W", 2, 0)
 	# whr.create_game("shusaku", "shusai", "W", 3, 0)
@@ -159,4 +278,6 @@ if __name__ == "__main__":
 	print(whr.ratings_for_player("shusaku"))
 	print(whr.ratings_for_player("shusai"))
 	whr.probability_future_match("shusai", "PF", 0)
+	print(whr.log_likelihood())
+	whr.print_ordered_ratings()
 
